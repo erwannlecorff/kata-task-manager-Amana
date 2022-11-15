4812b07ae030ad78691fb253983f132fd04117ab
EnzoFilangi <enzo.filangi@efrei.net>
2022-11-15 10:53:34 +0100

    TaskStore can return a copy of its tasks

--- taskStore.js
+++ taskStore.js
@@ -4,6 +4,10 @@ class TaskStore {
     addTask(newTask){
         this.taskList.push(newTask)
     }
+
+    getTasks(){
+        return this.taskList.map((x) => x);
+    }
 }
 
 module.exports = { TaskStore }
--- tests/taskStore.test.js
+++ tests/taskStore.test.js
@@ -10,3 +10,14 @@ test("Store should keep track of new tasks", (t) => {
     t.ok(taskStore.taskList.includes(newTask), "The task list should contain the provided task")
     t.end()
 })
+
+test("Store should provide its internal list of tasks", (t) => {
+    const listOfTasks = ['A a', 'B b', 'C c']
+    const taskStore = new TaskStore();
+
+    taskStore.taskList = listOfTasks;
+
+    t.deepEqual(taskStore.getTasks(), listOfTasks)
+    t.notEquals(taskStore.getTasks(), listOfTasks, "The TaskStore should provide a copy of its internal list (and not the actual reference to the list)")
+    t.end()
+})
